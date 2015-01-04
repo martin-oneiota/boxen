fd594ade07647fd92da3ae82f54e3ed016a3c7fa
Ross McFarlane <ross@rossmcf.com>
2015-01-04 00:04:59 +0000

    Add personal manifest for rossmcf.

---
+++ modules/people/manifests/rossmcf.pp
@@ -0,0 +1,21 @@
+class people:rossmcf {
+
+  $home     = "/Users/${::boxen_user}"
+  $my       = "${home}/my"
+  $dotfiles = "${my}/dotfiles"
+
+  file { $my:
+    ensure  => directory
+  }
+
+#  repository { $dotfiles:
+#    source  => 'jbarnette/dotfiles',
+#    require => File[$my]
+#  }
+
+  include onepassword
+  include skype
+  include omnifocus
+  include textexpander
+
+}
