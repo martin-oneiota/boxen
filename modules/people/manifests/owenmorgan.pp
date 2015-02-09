f884a8b8ccdda9d8816b029a97d45a07d043147a
rossmcf <ross@rossmcf.com>
2015-02-09 10:07:28 +0000

    Add OS X defaults and Sublime for Owen.

--- modules/people/manifests/owenmorgan.pp
+++ modules/people/manifests/owenmorgan.pp
@@ -8,6 +8,17 @@ class people::owenmorgan {
   $home     = "/Users/${::boxen_user}"
   $dotfiles = "${home}/.dotfiles"
 
+  osx::global::expand_print_dialog
+  osx::dock::clear_dock
+  osx::dock::autohide
+  osx::dock::hide_indicator_lights
+  osx::finder::show_all_on_desktop
+  osx::finder::unhide_library
+  class { 'osx::mouse::button_mode':
+      mode => 2
+  }
+  include sublime_text::v2
+
   #  repository { $dotfiles:
   #    source  => 'owenmorgan/dotfiles'
   #  }
