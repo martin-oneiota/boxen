e8de6381519ecffebddd1383f72cb73e7ead5aac
rossmcf <ross@rossmcf.com>
2015-04-08 15:39:49 +0100

    Switch new-starter-form project to github.

--- modules/projects/manifests/new-starter-form.pp
+++ modules/projects/manifests/new-starter-form.pp
@@ -1,6 +1,6 @@
 class projects::new-starter-form {
   php::project { 'new-starter-form':
-    source        => 'git@bitbucket.org:oneiota/new-starter-form.git',
+    source        => 'itsoneiota/new-starter-form',
     nginx         => 'php/nginx/nginx.conf.erb',
     php           => '5.4.11',
     mysql         => true,
